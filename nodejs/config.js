98fe8c755a153cf6453c00c0ad5d3a16168e06b0
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-12-09 15:25:13 +0530

    Create config.js

---
+++ nodejs/config.js
@@ -0,0 +1,10 @@
+// Hardcoded sensitive data (Security vulnerability)
+const API_KEY = "12345-ABCDE";
+
+// Exporting unused constant (Code smell)
+const UNUSED_CONSTANT = "I am not used";
+
+module.exports = {
+    API_KEY,
+    UNUSED_CONSTANT
+};
