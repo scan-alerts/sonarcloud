c23fe374c0c94ba8c5874e0d89e38ec606cf7193
rahulcoder010 <rahulauthtest@gmail.com>
2024-12-16 15:17:21 +0530

    Attached videos should have the thumbnail of the first opening image

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
+
 const express = require('express');
-const { add: addFunction, subtract: subtractFunction } = require('./mathOperations');
+const { add, subtract } = require('./mathOperations');
 const { logMessage } = require('./logger');
 
 const app = express();
 const port = 3000;
 
-app.disable("x-powered-by");
-
 app.get('/add', (req, res) => {
   const { a, b } = req.query;
-  const result = addFunction(Number(a), Number(b));
+  const result = add(Number(a), Number(b));
   logMessage(`Addition Result: ${result}`);
   res.send(`Addition Result: ${result}`);
 });
 
 app.get('/subtract', (req, res) => {
   const { a, b } = req.query;
-  const result = subtractFunction(Number(a), Number(b));
+  const result = subtract(Number(a), Number(b));
   logMessage(`Subtraction Result: ${result}`);
   res.send(`Subtraction Result: ${result}`);
 });
@@ -26,5 +25,7 @@ app.listen(port, () => {
 });
 
 // file: test.js
-console.log(addFunction(2, 3)); 
-console.log(subtractFunction(5, 2));
+const { add, subtract } = require('./mathOperations');
+
+console.log(add(2, 3)); 
+console.log(subtract(5, 2));
