b2a524bd3094f179525ca5ac3f5c3cbe19ace50f
rahulcoder010 <139946269+rahulcoder010@users.noreply.github.com>
2024-12-18 11:37:23 +0530

    Sonar-16 (task) chlid 1

--- utils.js
+++ utils.js
@@ -1,8 +1,14 @@
-function max(a, b) {
-  return a > b ? a : b;
-}
+const assert = require('assert');
+const { max, min } = require('./path_to_your_file');
 
-function min(a, b) {
-  return a < b ? a : b; 
-}
-module.exports = { max, min };
+describe('Testing max and min functions', () => {
+  it('should return maximum of two numbers', () => {
+    assert.strictEqual(max(10, 20), 20);
+    assert.strictEqual(max(100, 20), 100);
+  });
+
+  it('should return minimum of two numbers', () => {
+    assert.strictEqual(min(10, 20), 10);
+    assert.strictEqual(min(100, 20), 20);
+  });
+});
